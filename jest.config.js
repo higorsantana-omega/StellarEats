0ad90e40eaa14ca7cafc7a59f0603b452be1b7ac
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-06-03 19:57:50 -0300

    chore: configure jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  roots: ['<rootDir>/test'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!**/test/**'
+  ],
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1'
+  }
+}
