4117e5e2e82082a0c0d15fcb85e82f6a02d4f0b9
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-06-04 02:15:52 -0300

    feat: implement hash compare

--- src/toolbox/IToolbox.ts
+++ src/toolbox/IToolbox.ts
@@ -4,6 +4,7 @@ interface UUID {
 
 interface Encryption {
   encrypt(data: string): string
+  hashCompare(correctPassword: string, enteredPassword: string): boolean
 }
 
 export default interface ToolBox extends UUID, Encryption {}
--- src/toolbox/toolbox.ts
+++ src/toolbox/toolbox.ts
@@ -5,6 +5,10 @@ import * as uuid from 'uuid'
 import bcrypt from 'bcrypt'
 
 class ToolBoxImpl implements ToolBox {
+  hashCompare (correctPassword: string, enteredPassword: string): boolean {
+    return bcrypt.compareSync(enteredPassword, correctPassword)
+  }
+
   encrypt (data: string): string {
     return bcrypt.hashSync(data, 10)
   }
