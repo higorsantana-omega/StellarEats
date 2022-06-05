a3a55c45f7a9875a234c185cd7cf3a3b26884703
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-06-05 00:43:41 -0300

    feat: new instance of repository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -2,9 +2,11 @@ import createMongoRepositories from './mongodb'
 import Repository from './Repository'
 
 import Account from '@/useCases/account/Account'
+import Restaurant from '@/useCases/restaurant/Restaurant'
 
 export interface Repositories {
   account: Repository<Account>
+  restaurant: Repository<Restaurant>
 }
 
 export default async function createRepositories (): Promise<Repositories> {
--- src/repositories/mongodb/index.ts
+++ src/repositories/mongodb/index.ts
@@ -3,6 +3,7 @@ import MongoRepository from './MongoRepository'
 import { Repositories } from '..'
 
 import Account from '@/useCases/account/Account'
+import Restaurant from '@/useCases/restaurant/Restaurant'
 
 export default async function createMongoRepositories (): Promise<Repositories> {
   const client = new MongoClient(process.env.MONGODB_URL, {
@@ -13,6 +14,7 @@ export default async function createMongoRepositories (): Promise<Repositories> 
   const database = client.db('uaifood_test')
 
   return {
-    account: new MongoRepository<Account>('userID', 'account', database)
+    account: new MongoRepository<Account>('userID', 'account', database),
+    restaurant: new MongoRepository<Restaurant>('restaurantID', 'restaurant', database)
   }
 }
