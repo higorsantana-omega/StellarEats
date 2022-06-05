1389fca5826643705c9df782727d9b3731ec4dea
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-06-05 20:04:40 -0300

    test: register restaurant controller

---
+++ test/controllers/restaurant/RegisterRestaurantController.test.ts
@@ -0,0 +1,30 @@
+import Authentication from '@/app/middleware/Authentication'
+import application from '@test/application.spec'
+import { createAccount } from '@test/useCases/account/utils'
+import request from 'supertest'
+
+describe('Signup Controller', () => {
+  test('should return an account on success', async () => {
+    const account = await createAccount()
+    const token = (new Authentication()).createAccessToken(account.userID)
+
+    await request(application.app)
+      .post('/restaurant')
+      .set('Authorization', token)
+      .send({
+        name: 'Uai food 3',
+        gastronomy: 'Brasileira',
+        phone: '11963367221',
+        address: {
+          postcode: '0353010',
+          street: 'Rua tal',
+          number: '120',
+          complement: '',
+          district: 'Bairro tal',
+          city: 'Minas gerais',
+          state: 'MG'
+        }
+      })
+      .expect(201)
+  })
+})
