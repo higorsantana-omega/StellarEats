bfc9b9abcca82f39aa0886205e3ac568c16d68fb
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-06-05 16:46:25 -0300

    feat: expected request when user signup

--- src/app/controllers/account/SignupController.ts
+++ src/app/controllers/account/SignupController.ts
@@ -2,6 +2,15 @@
 import { BaseController, Request, Response } from '../BaseController'
 
 export default class SignupController extends BaseController {
+  protected expectedRequest: {} = {
+    body: {
+      name: 'required|string',
+      email: 'required|string',
+      phone: 'required|string',
+      password: 'required|string'
+    }
+  }
+
   protected async execute (request: Request, response: Response): Promise<Response> {
     const account = await this.interactors
       .account
